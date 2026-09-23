89cb05047b1dac205885845b32f9f92d95082044
AlfredWilmot <18071042+AlfredWilmot@users.noreply.github.com>
2026-09-23 23:02:42 +0100

    fix: remove redundant debug Println

--- scripts/hms.go
+++ scripts/hms.go
@@ -103,7 +103,6 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	fmt.Println(records)
 
 	// populate template and dump to stdout
 	if err := records.Dump(hmsGoTemplate, os.Stdout); err != nil {
