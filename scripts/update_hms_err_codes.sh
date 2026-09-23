1124b4ea7c9dab209287479ce408fa8837a24b4d
AlfredWilmot <18071042+AlfredWilmot@users.noreply.github.com>
2026-09-23 23:03:20 +0100

    feat: shell script redirecting stdout from hms.go to errors.go

---
+++ scripts/update_hms_err_codes.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+set -e
+
+SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
+ROOT="$(dirname "${SCRIPT_DIR}")"
+OUT_FILE_PATH="${ROOT}/internal/hms/errors.go"
+
+go run "${SCRIPT_DIR}/hms.go" > "${OUT_FILE_PATH}"
